bda46a425076a31bdc6b6d31476e9b1c3af8afec
Nishan Bista <nishanbista525@gmail.com>
2024-06-18 15:59:01 +0545

    utils

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connect.js";
+import { app } from "./app.js";
 
-console.log(process.env.MONGODB_URL)
+console.log(process.env.MONGODB_URL);
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(`Error on express, ${error}`);
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server Running on PORT: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB CONNECTION FAILED!! ", err);
+  });
